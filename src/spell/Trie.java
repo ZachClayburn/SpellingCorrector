2afedd237b1ea760f42d0dc3b3aea6360535f448
Zach <zachclayburn@gmail.com>
2018-01-06 12:52:08 -0700

    Set up new implementing files

---
+++ src/spell/Trie.java
@@ -0,0 +1,73 @@
+package spell;
+
+public class Trie implements ITrie{
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    /**
+     * Adds the specified word to the trie (if necessary) and increments the word's frequency count
+     *
+     * @param word The word being added to the trie
+     */
+    @Override
+    public void add(String word) {
+
+    }
+
+    /**
+     * Searches the trie for the specified word
+     *
+     * @param word The word being searched for
+     * @return A reference to the trie node that represents the word,
+     * or null if the word is not in the trie
+     */
+    @Override
+    public INode find(String word) {
+        return null;
+    }
+
+    /**
+     * Returns the number of unique words in the trie
+     *
+     * @return The number of unique words in the trie
+     */
+    @Override
+    public int getWordCount() {
+        return 0;
+    }
+
+    /**
+     * Returns the number of nodes in the trie
+     *
+     * @return The number of nodes in the trie
+     */
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+
+    class Node implements INode{
+        /**
+         * Returns the frequency count for the word represented by the node
+         *
+         * @return The frequency count for the word represented by the node
+         */
+        @Override
+        public int getValue() {
+            return 0;
+        }
+    }
+}
